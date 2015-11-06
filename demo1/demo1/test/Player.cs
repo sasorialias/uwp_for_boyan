d38c818b83a83cb8c541e83149c48a5a63dc7362
sasorialias <wwwhuyalong@163.com>
2015-11-07 00:05:55 +0800

    hehe

---
+++ demo1/demo1/test/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    /// <summary>
+    /// 总的类
+    /// </summary>
+    class Player
+    {
+    }
+}
